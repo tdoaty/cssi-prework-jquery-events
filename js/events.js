5270909e4991e057db55b83f2a463cf9ca327c46
tdoaty <tdoaty@google.com>
2017-04-18 09:50:13 -0400

    need to fix submitIt function

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
 
+function getIt(){
+  $('p').click(function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+  $('img').addClass('tasty')
+}
+
+function pressIt(){
+  $("input").keydown(function(event){
+    $("input").html("Key: g" + event.which);
+    alert("You pressed the g key!")
+  });
+}
+
+function sumbitIt(){
+  $(this).click(function(){
+    alert("Your form is going to be submitted now")
+  });
+  return;
+}
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+sumbitIt();
 });
